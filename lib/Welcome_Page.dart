1e6b9cd7741f3523824ffc7f0684e7e89b8937f5
dipeshrawal <dipeshrawal999@gmail.com>
2023-12-29 16:29:20 +0545

    dipesh update

--- lib/Welcome_Page.dart
+++ lib/Welcome_Page.dart
@@ -37,91 +37,91 @@ class welcomepage_state extends State<Welcome_Page>{
                   ),
                 ),
               ),
-              Center(
-                child: Padding(
-                  padding: const EdgeInsets.only(left:5, right: 20),
-                  child: Positioned(
-                    bottom: 305,
-                    left: 35,
-                    child: Container(
-                      width: 360,
-                      height: 190,
-                      child: Stack(
-                        children: [
-                          Positioned(
-                            left: 0,
-                            top: 0,
-                            child: SizedBox(
-                              width: 210.59,
-                              height: 32,
-                              child: Text(
-                                'Welcome to ',
-                                style: TextStyle(
-                                  color: Color(0xFF21205B),
-                                  fontSize: 25,
-                                  fontStyle: FontStyle.italic,
-                                  fontFamily: 'ABeeZee',
-                                  fontWeight: FontWeight.w400,
-                                  height: 0,
+              Padding(
+                padding: const EdgeInsets.only(left:20, right: 20),
+                child: Center(
+                    child: Positioned(
+                      bottom: 305,
+                      left: 35,
+                      child: Container(
+                        width: 360,
+                        height: 190,
+                        child: Stack(
+                          children: [
+                            Positioned(
+                              left: 0,
+                              top: 0,
+                              child: SizedBox(
+                                width: 210.59,
+                                height: 32,
+                                child: Text(
+                                  'Welcome to ',
+                                  style: TextStyle(
+                                    color: Color(0xFF21205B),
+                                    fontSize: 25,
+                                    fontStyle: FontStyle.italic,
+                                    fontFamily: 'ABeeZee',
+                                    fontWeight: FontWeight.w400,
+                                    height: 0,
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                          Positioned(
-                            left: 90,
-                            top: 26,
-                            child: Container(
-                              width: 58,
-                              height: 45,
-                              decoration: BoxDecoration(
-                                image: DecorationImage(
-                                  image: AssetImage('assets/images/logo.png'),
-                                  fit: BoxFit.fill,
+                            Positioned(
+                              left: 90,
+                              top: 26,
+                              child: Container(
+                                width: 58,
+                                height: 45,
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                    image: AssetImage('assets/images/logo.png'),
+                                    fit: BoxFit.fill,
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                          Positioned(
-                            left: 138,
-                            top: 30,
-                            child: SizedBox(
-                              width: 245.88,
-                              height: 60,
-                              child: Text(
-                                'Hostel Finder\n',
-                                style: TextStyle(
-                                  color: Color(0xFF14223B),
-                                  fontSize: 30,
-                                  fontFamily: 'Hind Jalandhar',
-                                  fontWeight: FontWeight.w700,
-                                  height: 0,
+                            Positioned(
+                              left: 138,
+                              top: 30,
+                              child: SizedBox(
+                                width: 245.88,
+                                height: 60,
+                                child: Text(
+                                  'Hostel Finder\n',
+                                  style: TextStyle(
+                                    color: Color(0xFF14223B),
+                                    fontSize: 30,
+                                    fontFamily: 'Hind Jalandhar',
+                                    fontWeight: FontWeight.w700,
+                                    height: 0,
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                          Positioned(
-                            left: 0,
-                            top: 100,
-                            child: Container(
-                              width: 360,
-                              height: 100,
-                              child: Text(
-                                'Find the Hostel, list your Hostel in just a simple steps, in your hand. \n \nYou are one step away.',
-                                style: TextStyle(
-                                  color: Color(0xFF7A7F9E),
-                                  fontSize: 14,
-                                  fontFamily: 'Hind',
-                                  fontWeight: FontWeight.w400,
-                                  height:1.5,
+                            Positioned(
+                              left: 0,
+                              top: 100,
+                              child: Container(
+                                width: 360,
+                                height: 100,
+                                child: Text(
+                                  'Find the Hostel, list your Hostel in just a simple steps, in your hand. \n \nYou are one step away.',
+                                  style: TextStyle(
+                                    color: Color(0xFF7A7F9E),
+                                    fontSize: 14,
+                                    fontFamily: 'Hind',
+                                    fontWeight: FontWeight.w400,
+                                    height:1.5,
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                        ],
-                      ),
-                    )
+                          ],
+                        ),
+                      )
+                    ),
                   ),
-                ),
               ),
               Positioned(
                   bottom: 150,
